68f4f7088c49764147eec22e82e8778a1fd0c8b3
GanjikuntaLalasa <lalasaganjikunta74@gmail.com>
2026-01-20 22:24:23 +0530

    Update cms.js

---
+++ cms.js
@@ -0,0 +1,10 @@
+function signup(){
+    let username=document.getElementById("username").vlaue
+    let Email=document.getElementById("Email").vlaue
+    if(username=="239y1a0557@ksrmce.ac.in" && password=="admin" && password===confirmpassword){
+        alert("login successfull");
+    }
+    else{
+        alert("login failed")
+    }
+}
